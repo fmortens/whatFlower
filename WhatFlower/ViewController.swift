be1e3c5ddcdab8d0cd0e19af9434af334d613c8e
Frank Mortensen <fmortens@me.com>
2019-07-08 13:34:24 +0200

    Add starting point code for image picker

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -8,13 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var photoLibraryButton: UIBarButtonItem!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
     }
 
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            fatalError("Could not get image!")
+        }
+        
+        imageView.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func cameraButtonTapped(_ sender: UIBarButtonItem) {
+        
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func photoLibraryButtonTapped(_ sender: UIBarButtonItem) {
+        
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
 }
 
