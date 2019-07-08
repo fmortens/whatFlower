f6a3ae3eadff51e8aa86e0bde61f8d63181e3fac
Frank Mortensen <fmortens@me.com>
2019-07-08 16:13:45 +0200

    Add fetching and display of info text from wiki

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -17,8 +17,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var photoLibraryButton: UIBarButtonItem!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textView: UITextView!
     
     let imagePicker = UIImagePickerController()
+    let wikipediaURl = "https://en.wikipedia.org/w/api.php"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,51 +57,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         let request = VNCoreMLRequest(model: model) { (request, error) in
             
-            if error != nil {
-                fatalError("Could not process image \(error!)")
-            }
-            
-            guard let results = request.results as? [VNClassificationObservation] else {
+            guard let classification = request.results?.first as? VNClassificationObservation else {
                 fatalError("Model failed to process image")
             }
             
-            guard let flowerName = results.first?.identifier else {
-                fatalError("Could not get result")
-            }
+            self.navigationItem.title = classification.identifier.capitalized
             
-            self.navigationItem.title = flowerName.capitalized
-            
-            // Will probably move this to a new view showing details, but for now
-            let wikipediaURl = "https://en.wikipedia.org/w/api.php"
-            
-            let parameters : [String:String] = [
-                "format" : "json",
-                "action" : "query",
-                "prop" : "extracts",
-                "exintro" : "",
-                "explaintext" : "",
-                "titles" : flowerName,
-                "indexpageids" : "",
-                "redirects" : "1",
-            ]
-
-            Alamofire
-                .request(wikipediaURl, method: .get, parameters: parameters)
-                .responseJSON {
-                    response in
-                    if response.result.isSuccess {
-                        
-                        print("Success! Got wikipedia data.")
-                        
-                        let wikiJSON : JSON = JSON(response.result.value!)
-                        
-                        print(wikiJSON)
-                        
-                    }
-                    else {
-                        print("Error: \(response.result.error!)")
-                    }
-            }
+            self.lookupFlowerInformationFromWikipedia(flowerName: classification.identifier)
             
         }
         
@@ -109,6 +73,39 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             try handler.perform([request])
         } catch {
             print("Could not perform request \(error)")
+        }
+        
+    }
+    
+    func lookupFlowerInformationFromWikipedia(flowerName: String) {
+        
+        let parameters : [String:String] = [
+            "format" : "json",
+            "action" : "query",
+            "prop" : "extracts",
+            "exintro" : "",
+            "explaintext" : "",
+            "titles" : flowerName,
+            "indexpageids" : "",
+            "redirects" : "1",
+        ]
+        
+        Alamofire
+            .request(wikipediaURl, method: .get, parameters: parameters)
+            .responseJSON {
+                response in
+                if response.result.isSuccess {
+                    
+                    let wikiJSON : JSON = JSON(response.result.value!)
+                    let pageId = wikiJSON["query"]["pageids"][0].stringValue
+                    let extract = wikiJSON["query"]["pages"][pageId]["extract"].stringValue
+                    
+                    self.textView.text = extract
+                    
+                }
+                else {
+                    print("Error: \(response.result.error!)")
+                }
         }
         
     }
